f7f65b42fbc0b183b0354eb94fca67351efee2db
Vlad Doronenko <harveyspectr46@gmail.com>
2026-04-25 22:21:39 +0300

    Fix: Enable vertical scrolling on mobile and PageTransition minHeight

--- src/components/PageTransition.jsx
+++ src/components/PageTransition.jsx
@@ -37,7 +37,7 @@ export default function PageTransition({ children }) {
             exit="out"
             variants={pageVariants}
             transition={pageTransition}
-            style={{ width: '100%', height: '100%' }}
+            style={{ width: '100%', minHeight: '100%' }}
         >
             {children}
         </motion.div>
