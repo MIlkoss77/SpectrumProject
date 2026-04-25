a420ab1413053005e2bc4f1ae5de0fd65a674ebf
Vlad Doronenko <harveyspectr46@gmail.com>
2026-04-25 21:52:15 +0300

    Downgrade: Revert to Prisma 5 for stability

--- server/config/database.js
+++ server/config/database.js
@@ -1,10 +1,7 @@
 import pkg from '@prisma/client';
 const { PrismaClient } = pkg;
 
-// In Prisma 7, we pass the URL directly to the constructor
-const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL || 'file:./dev.db'
-});
+const prisma = new PrismaClient();
 
 export default prisma;
 export { prisma };
